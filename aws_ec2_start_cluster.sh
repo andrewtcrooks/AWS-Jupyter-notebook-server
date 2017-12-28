58606216f9cba19651aba43a3a4fa0a015825580
Andrew Crooks <andrewtcrooks@gmail.com>
2017-12-27 19:43:30 -0800

    updated AWS Jupyter server sh script with lines cut to 72 char width

--- aws_ec2_start_cluster.sh
+++ aws_ec2_start_cluster.sh
@@ -8,22 +8,31 @@
 ##-------------------------------------##
 
 # CHANGE THE PARAMETERS BELOW TO FIT YOUR PROJECT
-KEY="merlin" # (Change) your aws keypair file name sans the ".pem"
-KEY_LOC="/Users/andrew/git/andrewtcrooks.com/content/notebooks" # (Change) key file loc (e.g. the *.pem file)
-INSTANCE_TYPE="t2.micro" # (Optional Change) small to start with, move to bigger like c4.8xlarge when needed
+KEY="merlin" # AWS keypair file name sans the ".pem"
+KEY_LOC="/Users/andrew/git/andrewtcrooks.com/content/notebooks"
 
-PORT="8888" #  DON'T CHANGE!: the localhost port where your jupyter notebook will be served.
-            #   If your local jupyter server is already on 8888, change your local server port
-            #   to something else like 8889.
-            #  REASON: AWS automatically serves jupyter on port 8888 and you only want to have
-            #   to type 'jupyter notebook' instead of 'jupyter notebook --port:8887' after
-            #   ssh-ing into the server
-IMAGEID="ami-f1e73689" # (Don't Change) Deep Learning AMI with Conda (Ubuntu)
+# OPTIONAL CHANGE
+INSTANCE_TYPE="t2.micro" # Small instance to start with
+                         # Move to bigger like c4.8xlarge when needed
+IMAGEID="ami-f1e73689" # Deep Learning AMI with Conda (Ubuntu)
+REGION="us-west-2" # (Change if NOT in Oregon or Washington state)
+
+# DON'T CHANGE
+PORT="8888" #  The localhost port where remote jupyter notebook will be 
+            #   served.
+            #  If you have a local jupyter server that is already on 
+            #   8888, change your local server port to something else 
+            #   like 8889.
+            #  REASON: AWS automatically serves jupyter on port 8888 
+            #   and you only want to have to type 'jupyter notebook'
+            #   instead of 'jupyter notebook --port:8887' after ssh-ing 
+            #   into the server
 COUNT="1" # (Don't Change) Number of instances to create
-REGION="us-west-2" # (Don't change if you are in Seattle!)
 USER="ubuntu" # (Don't Change)the EC2 linux user name
-UD="" # supposed to be boot script but doesn't seem to work. Solution, use AMI with everything preinstalled
-#UD="--user-data file://$HOME/scripts/aws/start.txt"
+
+# UD is supposed to be boot script but it doesn't seem to work. 
+# Left it blank and used AMI with everything preinstalled instead.
+UD="" #UD="--user-data file://$HOME/scripts/aws/start.txt"
 BDM="--block-device-mappings file://mappings.json" 
 
 
@@ -34,7 +43,9 @@ BDM="--block-device-mappings file://mappings.json"
 
 # start EC2 instance using above parameters
 # and save instance id to variable INSTANCE
-INSTANCE="$(aws ec2 run-instances --image-id $IMAGEID --instance-type $INSTANCE_TYPE --count $COUNT --key-name $KEY --region $REGION --query 'Instances[0].InstanceId' $BDM $UD)"
+INSTANCE="$(aws ec2 run-instances --image-id $IMAGEID --instance-type \
+            $INSTANCE_TYPE --count $COUNT --key-name $KEY --region \
+            $REGION --query 'Instances[0].InstanceId' $BDM $UD)"
 
 # remove quotes around INSTANCE id
 INSTANCE="${INSTANCE%\"}"
@@ -46,8 +57,8 @@ INSTANCE="${INSTANCE#\"}"
 ## Verify Instance ##
 ##-----------------##
 
-# seems to take about 3 to 4 minutes for SSH fingerprints to show
-# up in the output. wait for 2 and a half minutes, then start polling output
+# seems to take about 5-6 minutes for SSH fingerprints to show
+# up in the output. wait for 2.5 minutes, then start polling output
 #echo $'\n'
 echo "Waiting for $INSTANCE to boot"
 echo ""
@@ -56,9 +67,11 @@ i=0
 while [ 1 ]
 do
 
-FINGERPRINTS=$(aws ec2 get-console-output --instance-id $INSTANCE | egrep -m 1 -o '([0-9a-f][0-9a-f]:){15}[0-9a-f][0-9a-f]')
+FINGERPRINTS=$(aws ec2 get-console-output --instance-id $INSTANCE | \
+                egrep -m 1 -o '([0-9a-f][0-9a-f]:){15}[0-9a-f][0-9a-f]')
 
-SSH_SERVER_KEY=$(aws ec2 get-console-output --instance-id $INSTANCE | sed -n 's/^.*nssh-rsa //p' | sed 's/ root@ip-.*$//g')
+SSH_SERVER_KEY=$(aws ec2 get-console-output --instance-id $INSTANCE | \
+                sed -n 's/^.*nssh-rsa //p' | sed 's/ root@ip-.*$//g')
 
 
 # Check for FINGERPRINT every ~10 seconds (9 + runtime)
@@ -81,16 +94,22 @@ echo ""
 echo "Expected fingerprints are $FINGERPRINTS"
 
 # get hostname for the instance
-HOST=$(aws ec2 describe-instances | grep -m 1 us-west-2.compute.amazonaws.com | egrep -o 'ec2(-[0-9]+){4}.us-west-2.compute.amazonaws.com')
+HOST=$(aws ec2 describe-instances | \
+        grep -m 1 us-west-2.compute.amazonaws.com | \
+        egrep -o 'ec2(-[0-9]+){4}.us-west-2.compute.amazonaws.com')
 HOST_IP=$(echo "$HOST" | sed "s/ec2-//g;s/\..*//g;s/-/./g")
 HOST_ALIAS="aws-ec2"
 echo "Host is $HOST located at $HOST_IP"
 
 #cat host.key >> ~/.ssh/known_hosts 2>/dev/null
 
-# Read the private OpenSSH format from the *.pem file and output public key to host.key
-ssh-keygen -yf $KEY.pem > host.key
-# Output the fingerprint of the specified public key to host.fingerprint.
+# ensure proper permissions are set on .pem file
+chmod 400 $KEY_LOC/$KEY.pem
+
+# Read the private OpenSSH format from the *.pem file 
+# and output public key to host.key
+ssh-keygen -yf $KEY_LOC/$KEY.pem > host.key
+# Output the fingerprint of the public key to host.fingerprint.
 ssh-keygen -lf host.key > host.fingerprint
 # Store fingerprint from host.fingerprint into bash var for printing
 read len ACTUAL_FINGERPRINTS host rsa < host.fingerprint
@@ -140,7 +159,7 @@ echo "Uploading Data"
 echo ""
 
 # Copy jupyter custom config files to instance
-scp -i $KEY.pem -rq ~/.jupyter ubuntu@$HOST:~/.jupyter
+scp -i $KEY_LOC/$KEY.pem -rq ~/.jupyter ubuntu@$HOST:~/.jupyter
 
 # # Copy installation script to instance since --data-file just hangs 
 # scp -i $KEY.pem ~/scripts/aws/start.txt ubuntu@$HOST:~/start
@@ -177,7 +196,8 @@ echo ""
 echo ""
 echo "  To end session cleanly, in the same terminal:"
 echo ""
-echo "    1. Type (Ctrl + C) to kill notebook server and enter 'y' to confirm"
+echo "    1. Type (Ctrl + C) to kill notebook server and enter 'y' to \
+confirm"
 echo ""
 echo "    2. Then run:"
 echo ""
@@ -193,4 +213,3 @@ else
 echo "Fingerprints do not match"
 
 fi
-
